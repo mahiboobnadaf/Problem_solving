45775f33439a771127c7d0bc6b7fe127165131c8
Mahiboob Nadaf <127092821+mahiboobnadaf@users.noreply.github.com>
2024-09-07 17:49:51 +0530

    fibonacci numbers and traingle printing with *

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+// console.log("Mahiboob")
+let prev=0
+let next=1
+for(let i=0;i<10;i++){
+    // console.log(i)
+    
+    let fib=prev+next;
+    console.log(prev)
+    prev=next;
+    next=fib
+    
+}
---
+++ trainglePrint.js
@@ -0,0 +1,13 @@
+
+for(let i=0;i<=6;i++){
+
+    let a=""
+
+    for(let j = 6-i ; j> 0 ; j--){
+        a += " "
+    }
+    console.log(a + "* ".repeat(i))  
+}
+
+
+
