a3aef19ef0d7265fbbb5add5d070ec998d445078
Mahiboob Nadaf <127092821+mahiboobnadaf@users.noreply.github.com>
2024-09-08 15:50:06 +0530

    Printing Multiplication Tables program

---
+++ printingTables.js
@@ -0,0 +1,11 @@
+// let num = 22
+for(let j=2;j<=12;j++){
+
+    let num=j
+
+    for(let i=1;i<=10;i++){
+        res = num * i
+        console.log(` ${num} * ${i} = ${res} `)
+    }
+    console.log("\n")
+}
