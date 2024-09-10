02ca2453f4bb9c1fceef7caebf653c40b2fdad07
mahiboobnadaf <rasool97260@gmail.com>
2024-09-10 14:23:45 +0530

    currency

---
+++ possibeCurrency.js
@@ -0,0 +1,8 @@
+const AMOUNT = 20050;
+const notes = [100,200,500];
+
+console.log(`100 -> ${AMOUNT/notes[0]} notes `)
+console.log(`200 -> ${AMOUNT/notes[1]} notes `)
+console.log(`500 -> ${AMOUNT/notes[2]} notes `)
+
+
