5ff8dacd87ba2f26b05dd88aed5bf156f6df9997
mahiboobnadaf <rasool97260@gmail.com>
2024-09-11 11:41:24 +0530

    var example extended2

--- var_vairable.js
+++ var_vairable.js
@@ -1,33 +1,33 @@
-// var z = 5
-// function foo(){
-//     var x = 1;
+var z = 5
+function foo(){
+    var x = 1;
 
-//     function bar(){
-//         var y = 2;
-//         x=3
-//         var z=7
+    function bar(){
+        var y = 2;
+        x=3
+        var z=7
         
-//         console.log(x)
-//         console.log(y)
-//         console.log(z)
+        console.log(x)
+        console.log(y)
+        console.log(z)
 
 
-//     }
-//     // console.log(y)
-//     bar()
-//     console.log(x)
-//     console.log(z)
+    }
+    // console.log(y)
+    bar()
+    console.log(x)
+    console.log(z)
 
 
 
-// }
-// foo()
+}
+foo()
 
-// var a=2;
-// var a=9;
-// console.log(a)
-// var a;
-// console.log(a)
+var a=2;
+var a=9;
+console.log(a)
+var a;
+console.log(a)
 
 var c =10;
 function c(){
