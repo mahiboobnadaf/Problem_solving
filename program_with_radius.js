2fea7aa23f7a1037ea39ea0af879611e8d38bca5
mahiboobnadaf <rasool97260@gmail.com>
2024-09-13 13:03:06 +0530

    Calculating Diameter  program

--- program_with_radius.js
+++ program_with_radius.js
@@ -20,3 +20,13 @@ const calculateCircumference= function(radius){
 }
 
 console.log(calculateCircumference(radius))
+
+
+const calculateDiameter = function(radius){
+    const result3 = []
+    for(let i=0 ; i<radius.length;i++){
+        result3.push(2 * radius[i])
+    }
+    return result3
+}
+ console.log(calculateDiameter(radius))
