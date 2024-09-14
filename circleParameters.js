9eb4325e937deaad9b812e23fc91fe1e236ba79d
mahiboobnadaf <rasool97260@gmail.com>
2024-09-14 12:08:47 +0530

    calculation using Map

--- circleParameters.js
+++ circleParameters.js
@@ -17,9 +17,20 @@ const calculate= function(arr,logic){
     for(let i=0;i<arr.length;i++){
         result.push(logic(arr[i]))
     }
-    console.log(result)
+    return result
 }
 
 console.log(calculate(radius,area))
 console.log(calculate(radius,circumference))
 console.log(calculate(radius,diameter))
+
+
+// Another method to calculate the Area, circumference and diameter of circle
+
+const calculatebyMap = function(arr,logic){
+    return  arr.map(logic)
+}
+
+console.log(calculatebyMap(radius,area))
+console.log(calculatebyMap(radius,circumference))
+console.log(calculatebyMap(radius,diameter))
