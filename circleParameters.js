54c1d90f788ebbe0a7272f22d1a64f0d63fbfbbe
mahiboobnadaf <rasool97260@gmail.com>
2024-09-14 11:55:31 +0530

    calculating area, circumference, diameter of circle

--- circleParameters.js
+++ circleParameters.js
@@ -1,16 +1,25 @@
 const radius = [3,5,4,2]
 
 function area(radius){
-    return (Math.PI * radius * radius)
+    return Math.PI * radius * radius
 }
 
-const calculate= function(radius,logic){
+function circumference(radius){
+    return 2 * Math.PI * radius
+}
+
+function diameter(radius){
+    return 2 * radius
+}
+
+const calculate= function(arr,logic){
     const result=[]
-    for(let i=0;i<radius.length;i++){
-        // console.log(radius[i])
-        result.push(logic(radius[i]))
+    for(let i=0;i<arr.length;i++){
+        result.push(logic(arr[i]))
     }
     console.log(result)
 }
 
 console.log(calculate(radius,area))
+console.log(calculate(radius,circumference))
+console.log(calculate(radius,diameter))
