3660bcb6e778baafe12e381e680cddb576eafbe3
mahiboobnadaf <rasool97260@gmail.com>
2024-09-15 17:24:56 +0530

    Block scope example added

---
+++ blockScopeExample.js
@@ -0,0 +1,13 @@
+//Block Scope Example learning
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
