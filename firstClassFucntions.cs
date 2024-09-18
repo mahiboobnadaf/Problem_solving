5736a4ea4cfe22bb1744f986bf2ee14d1206ff69
mahiboobnadaf <rasool97260@gmail.com>
2024-09-18 16:36:52 +0530

    First Class Function Defination added

---
+++ firstClassFucntions.cs
@@ -0,0 +1,5 @@
+// First Class Fucntions or First class Citizens
+
+    // The abiity of a function to be used as variable values and can be passed as arguments to another functions 
+    // and can be returned from the functions is called First class Fucntions.
+
