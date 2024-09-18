e1438bf066eddc9f9fa182250c4c3bdc5f768ff1
mahiboobnadaf <rasool97260@gmail.com>
2024-09-18 16:32:11 +0530

    Named Function expression

--- dif_Functions.js
+++ dif_Functions.js
@@ -11,3 +11,22 @@ var b = function (){
     console.log("b is called");
 }
 b();
+
+// Anonymous Fucntion -> a function which do not have function_name
+
+        // function (){
+        //    ...
+        // }
+
+var c = function (){
+    console.log("b is called");
+}
+c();
+
+
+// Named Function Expression -> A function expression which has function_name
+
+var d= function xyz(){
+    console.log("d is called");
+}
+d();
