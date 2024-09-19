2eed8fbede7fc3a80e7c3d4da177ad26fd161908
mahiboobnadaf <rasool97260@gmail.com>
2024-09-19 12:30:48 +0530

    String concept

---
+++ stringLearing.js
@@ -0,0 +1,3 @@
+// Strings in Js are enclosed within  Singal quote '' ,Doble quote "" , Backtics `` 
+//Singal quote and Doube quote are singal line statements , 
+// Backtics are multiline statements
