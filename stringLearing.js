d8309cd7aec64e8be3815465703764a77cac711d
mahiboobnadaf <rasool97260@gmail.com>
2024-09-19 12:59:03 +0530

    Changing case of string

--- stringLearing.js
+++ stringLearing.js
@@ -6,6 +6,7 @@
 let str1 = "Hello Nadaf"
 
 // String.at(position) , String[position] - methods
+console.log('----String Positions-------')
 
 console.log(str1.at(6))     // N
 
@@ -20,13 +21,16 @@ console.log(str1.at(-1)) // f
 console.log(str1[-1]) // square brackets always return undefined for negativ indexes
 
 // Iterating using for..of
+console.log('----Iteration using for..of----')
 
 for(let char of str1){
     console.log(char)       //prints each character line by line
 }
 
-console.log('----------------')
+
+
 // Strings are immutable in Js , we cannot change a character
+console.log('----Strings are immutable-----')
 
 let str2 = "Hello" 
 str2[0] = 'h'
@@ -35,3 +39,10 @@ console.log(str2)       // Does Not change the string
 let str3 = "Hello"
 str3 =  str3[4] + 'h'   //Only appends the string
 console.log(str3)
+
+// Changing the case with methods toLowerCase()  and  toUpperCase()
+console.log('-----Changing case-----')
+
+console.log(str2.toLowerCase())
+
+console.log(str2.toUpperCase())
