d30206bb002f8b1cd115173a39be069581c69e9a
mahiboobnadaf <rasool97260@gmail.com>
2024-09-19 23:57:52 +0530

    Array push pop method added

---
+++ Array/arrayBasics_2.js
@@ -0,0 +1,28 @@
+// Methods pop/push, shift/unshift
+
+// A queue is one of the most common uses of an array. In computer science, this means an ordered collection of elements which supports two operations:
+
+// * push appends an element to the end.
+// * shift get an element from the beginning, advancing the queue, so that the 2nd element becomes the 1st.
+
+// There’s another use case for arrays – the data structure named stack.
+
+// It supports two operations:
+
+// * push adds an element to the end.
+// * pop takes an element from the end.
+
+// Arrays in JavaScript can work both as a queue and as a stack. They allow you to add/remove elements, both to/from the beginning or the end.
+
+// In computer science, the data structure that allows this, is called deque.
+
+// Pop
+
+let arr = ["Apple", "banana", "Mango"];
+arr.pop();              // removes last element
+console.log(arr);       //prints only ['Apple','banana']
+
+// Push
+
+arr.push("Dates");      // add 'Dates' at end of array
+console.log(arr)        // prints ['Apple','banana','Dates']
