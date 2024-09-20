5de090d32eb4eae473a62080b85aea4138c80752
mahiboobnadaf <rasool97260@gmail.com>
2024-09-20 12:11:19 +0530

    arra.length misuses

---
+++ Array/arrayLength.js
@@ -0,0 +1,13 @@
+let fruits = [];
+fruits[123] = "Apple";
+
+alert( fruits.length ); // 124
+
+//
+let arr = [1, 2, 3, 4, 5];
+
+arr.length = 2; // truncate to 2 elements
+alert( arr ); // [1, 2]
+
+arr.length = 5; // return length back
+alert( arr[3] ); // undefined: the values do not return
--- Array/loopArrays.js
+++ Array/loopArrays.js
@@ -8,7 +8,9 @@ console.log("-------------")
 
 for(let elem of arr){
     console.log(elem)
-}
+}   
+    // The for..of doesn’t give access to the number of the current element, 
+    // just its value, but in most cases that’s enough. And it’s shorter.
 
 console.log("-------------")
 
