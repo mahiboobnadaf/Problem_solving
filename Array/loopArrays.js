8cf9d19917ca903659e28e91ae5d10ce1b4697b0
mahiboobnadaf <rasool97260@gmail.com>
2024-09-20 12:07:08 +0530

    for in and for of loop example

--- Array/loopArrays.js
+++ Array/loopArrays.js
@@ -3,3 +3,15 @@ let arr = ['Apple', 'Banana', 'Mango'];
 for(let i=0;i<arr.length;i++){
     console.log(arr[i])
 }
+
+console.log("-------------")
+
+for(let elem of arr){
+    console.log(elem)
+}
+
+console.log("-------------")
+
+for(let elem in arr){
+    console.log(arr[elem])
+}
