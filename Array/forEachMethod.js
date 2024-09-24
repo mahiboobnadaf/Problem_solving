d1fc89ff1044d5a79fb242fcfd74b4a0ac67690a
mahiboobnadaf <rasool97260@gmail.com>
2024-09-24 14:56:39 +0530

    forEach method of array

---
+++ Array/forEachMethod.js
@@ -0,0 +1,12 @@
+// forEach() Method is an Iterative method. it provides callback function once for each array element
+// Syntax  array.forEach(callback Funciton(Item, index , array))
+
+['apple','mango','banana'].forEach((item,index,arr)=> {
+    console.log(item)
+    console.log(index)
+    console.log(arr)
+
+})
+
+// There is no way to stop or break a forEach() loop other than by throwing an exception. 
+// If you need such behavior, the forEach() method is the wrong tool.
