9836be33311f4681ec3e746566baf9f63f95179e
mahiboobnadaf <rasool97260@gmail.com>
2024-09-24 14:56:56 +0530

    forEach method of array

--- Array/forEachMethod.js
+++ Array/forEachMethod.js
@@ -10,3 +10,7 @@
 
 // There is no way to stop or break a forEach() loop other than by throwing an exception. 
 // If you need such behavior, the forEach() method is the wrong tool.
+
+//forEach() expects a synchronous function — it does not wait for promises.
+// Make sure you are aware of the implications while using promises (or async functions) as forEach callbacks.
+
