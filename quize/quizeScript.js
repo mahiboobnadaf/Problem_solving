9c5af62b01b9880864d73055f8aaea0aac8cda96
mahiboobnadaf <rasool97260@gmail.com>
2024-09-26 11:19:12 +0530

    preparing a quize game

---
+++ quize/quizeScript.js
@@ -0,0 +1,34 @@
+let que_ans = [
+    {
+    question : "What is your Year of Birth ?",
+    answer : [  {option : "2000",content : true},
+                {option : "1990",content : false},
+                {option : "1995",content : false},
+                {option : "1998",content : false},
+    ]
+},
+{
+    question : "What is your College Name ?",
+    answer : [  {option : "Jain",content : false},
+                {option : "KLE",content : false},
+                {option : "BVB",content : false},
+                {option : "GPT",content : true},
+    ]
+},
+{
+    question : "What is your Percentage ?",
+    answer : [  {option : "70",content : false},
+                {option : "90",content : false},
+                {option : "80",content : true},
+                {option : "95",content : false},
+    ]
+},
+{
+    question : "What is your Home Town ?",
+    answer : [  {option : "Hubli",content : true},
+                {option : "Bagalkot",content : false},
+                {option : "Savadatti",content : false},
+                {option : "Bangalore",content : false},
+    ]
+},
+]
