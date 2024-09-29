12827dc7feddeea34ee2338975feee339e1b7753
mahiboobnadaf <rasool97260@gmail.com>
2024-09-29 19:24:04 +0530

    sortArray example 2

--- Array/sortArray.js
+++ Array/sortArray.js
@@ -1,3 +1,14 @@
 let arr = [10,1,2,18,21];
-arr.sort()
-console.log(arr);  [1,10,18,2,21]
+let arr2 = ["Its","a","Sunny","day",'&',29, "holiday"];
+
+// arr.sort((a,b)=> a-b)
+arr2.sort()
+
+console.log(arr);  // [1,10,18,2,21]
+// console.log(arr2);  // [ '&', 29, 'Holiday', 'Sunny', 'a', 'day', "it's" ]
+
+
+console.log(arr.sort((a,b)=>{
+    console.log(`a:${a},b:${b}`)
+    return a-b;
+}))
