2e16d7dc00698bf532f39e9041c57ed849f0e35a
mahiboobnadaf <rasool97260@gmail.com>
2024-09-30 15:38:13 +0530

    split method examples

---
+++ Array/split_into_Array.js
@@ -0,0 +1,10 @@
+
+let str = "Mahiboob, Nadaf, is, going, to, Hyderabad"
+let splitted = str.split(', ')
+console.log(splitted)
+
+let splitted_2 = str.split(', ',3)
+console.log(splitted_2)     //Only 3 words returned
+
+let split_letters = str.split('')
+console.log(split_letters)
