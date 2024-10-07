885ecfd68aba554796cefc6d00dd170fc22d056f
Mahiboob Nadaf <127092821+mahiboobnadaf@users.noreply.github.com>
2024-10-07 14:17:33 +0530

    hjh

---
+++ rhombusStar.js
@@ -0,0 +1,23 @@
+
+for(let i=0;i<=6;i++){
+    let a=""
+    for(let j = 6-i ; j> 0 ; j--){
+        a += " "
+        // console.log(a)
+    }
+    console.log(a + "* ".repeat(i))  
+}
+
+for(let i=5;i>=0;i--){
+    let a=""
+    for(let j = 6-i ; j> 0 ; j--){
+        a += " "
+    }
+    console.log(a + "* ".repeat(i))  
+}
+
+
+
+
+
+
